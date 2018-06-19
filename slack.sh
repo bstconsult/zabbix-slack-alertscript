44ea370c12c80819a3fed4d9a52d6e90fc2eb95c
Todor Dragnev <todor@bstconsult.com>
2018-06-20 02:28:58 +0300

    refactoring

--- slack.sh
+++ slack.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Slack incoming web-hook URL and user name, https://my.slack.com/services/new/incoming-webhook/'
+# Slack incoming web-hook URL and user name
 url='CHANGEME'         # example: https://hooks.slack.com/services/QW3R7Y/D34DC0D3/BCADFGabcDEF123
 username='Zabbix'
-
-fallback="fallback message"
-pretext="pretext message"
 
 ## Values received by this script:
 # To = $1 (Slack channel or user to send the message to, specified in the Zabbix web interface; "@username" or "#channel")
@@ -14,7 +11,6 @@ pretext="pretext message"
 
 # Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY)
 to="$1"
-
 subject="${2//\"/\\\"}"
 
 case "$subject" in
@@ -32,32 +28,27 @@ case "$subject" in
     color='warning'
     ;;
 esac
-
 # The message that we want to send to Slack is the "subject" value ($2 / $subject - that we got earlier)
 #  followed by the message that Zabbix actually sent us ($3)
 message="${3//\"/\\\"}"
 
 # Build our JSON payload and send it as a POST request to the Slack incoming web-hook URL
+# https://itclubs.slack.com/services/B33333V35#message_attachments
 payload=$(cat <<EOF
 {
-  "attachments":[
-  {
-    "fallback":"${fallback}",
-    "pretext":"${pretext}",
+  "attachments":
+  [{
+    "fallback":"${subject}",
+    "pretext":"${subject}",
     "color":"${color}",
-    "fields":[
-    {
+    "fields":[{
       "title":"${subject}",
       "value":"${message}",
       "short":false
-    }
-    ]
-  }
-  ]
+    }]
+  }]
 }
 EOF
 )
-
-/usr/bin/curl -m 5 -X POST \
-  --data "${payload}" $url \
-  -A 'zabbix-slack-alertscript / https://github.com/ericoc/zabbix-slack-alertscript'
+/usr/bin/curl -m 5 -X POST --data "${payload}" $url \
+  --user-agent 'zabbix-slack-alertscript / https://github.com/ericoc/zabbix-slack-alertscript'
